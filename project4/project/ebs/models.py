eda5b3b20d76418abc303334ae01625e1217ee61
rizwan <rizwan.shaikh@cuelogic.com>
2017-04-05 11:11:21 +0530

    changes done

--- project4/project/ebs/models.py
+++ project4/project/ebs/models.py
@@ -2,10 +2,13 @@ from __future__ import unicode_literals
 from django.contrib.auth.models import User 
 from django.conf import settings
 from django.core.mail import send_mail
+from django.core.mail import EmailMultiAlternatives
 from django.db import models
 from django.db.models.signals import pre_save, post_save
 from django.db.models import signals
 from django.dispatch import receiver
+from django.template.loader import render_to_string, get_template
+from django.utils.html import strip_tags
 
 
 
@@ -26,14 +29,14 @@ def send_notification(sender,instance, *args,**kwargs):
 	try:
 		if instance.is_active != User.objects.get(id=instance.id).is_active and instance.is_active==True:
  			print "created is"
- 			subject = 'Welcome To News Magzine'
-			message = ('Your account has been activated now.'
-				'You can login and add blogs. click here to login : http://'+settings.HOST)
- 			from_email = settings.EMAIL_HOST_USER
- 			send_mail(subject, message, from_email, 
- 						[instance.email], 
- 						fail_silently=True)
- 		else:
+			subject = 'Welcome To News Magzine'
+			html_content=render_to_string('ebs/welcomemail.html', {'HOST':settings.HOST})
+			text_content=strip_tags(html_content) 
+			from_email = settings.EMAIL_HOST_USER
+			msg = EmailMultiAlternatives(subject, text_content, from_email, [instance.email])	
+			msg.attach_alternative(html_content, "text/html")
+			msg.send()
+		else:
  			pass
  	except:
  		print "Canot send email"
