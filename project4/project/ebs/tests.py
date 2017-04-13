0813d31e71f3434994715a1f98693277c497bea5
rizwan <rizwan.shaikh@cuelogic.com>
2017-04-13 21:06:26 +0530

    done 80 character indent

--- project4/project/ebs/tests.py
+++ project4/project/ebs/tests.py
@@ -19,11 +19,14 @@ from .forms import UserForm, OrgForm, UserLoginForm
 
 class Test1(TestCase):
 	def create_user(self):
-		user=User.objects.create(username=os.environ['USERNAME'],email=os.environ['EMAIL_FORGOT'],password=os.environ['EMAIL_FORGOT'])
+		user=User.objects.create(username=os.environ['USERNAME'],
+								email=os.environ['EMAIL_FORGOT'],
+								password=os.environ['EMAIL_FORGOT'])
 		return user
 
-	def create_org(self, orgname=os.environ["ORG"]):
-		user=User.objects.create(username=os.environ['USERNAME'],password="os.environ['PASSWORD']")
+	def create_org(self, orgname=os.environ['ORG']):
+		user=User.objects.create(username=os.environ['USERNAME'],
+								password=os.environ['PASSWORD'])
 		return Organisation.objects.create(user=user,orgname=os.environ['ORG'])
 	
 	def test_Organisation(self):
@@ -33,7 +36,10 @@ class Test1(TestCase):
 
 
 	def test_valid_User_form(self):
-		data = {'username': os.environ['USERNAME'],'email':os.environ['EMAIL'],'password': os.environ['PASSWORD'],'password1':os.environ['PASSWORD']}
+		data = {'username': os.environ['USERNAME'],
+				'email':os.environ['EMAIL'],
+				'password': os.environ['PASSWORD'],
+				'password1':os.environ['PASSWORD']}
 		form = UserForm(data=data)
 		self.assertTrue(form.is_valid())
 
@@ -53,30 +59,54 @@ class Test1(TestCase):
 		img=File(open('/home/rizwan/Downloads/gile.jpg','r'))
 		img=str(img)
 		new_group,created = Group.objects.get_or_create(name='client')
-		response = client.post(reverse('register'), {'username': os.environ['USERNAME'],'email':os.environ['EMAIL'], 'password':os.environ['PASSWORD'],'password1':os.environ['PASSWORD'], 'orgname':os.environ['ORG'], 'orglogo':File(open('/home/rizwan/Downloads/gile.jpg','r'))})
+		response = client.post(reverse('register'), 
+									{'username': os.environ['USERNAME'],
+									'email':os.environ['EMAIL'], 
+									'password':os.environ['PASSWORD'],
+									'password1':os.environ['PASSWORD'], 
+									'orgname':os.environ['ORG'], 
+					'orglogo':File(open('/home/rizwan/Downloads/gile.jpg','r'))
+													}
+								)
 		self.assertTrue(response.status_code, 200)
 		self.assertContains(response,'{"status": "success", "message": "Your account has been created and pending for admin approval.  You will get an email after admin approval on your registered email id. This process will take 24 hours."}')
 
 	def test_register_fail(self):
 		client = Client()
 		new_group,created = Group.objects.get_or_create(name='client')
-		response = client.post(reverse('register'), {'username': os.environ['USERNAME'], 'email': os.environ['EMAIL'], 'password': os.environ['PASSWORD'],'password1':os.environ['PASSWORD'], 'orgname': os.environ['ORG'], 'orglogo':'/home/rizwan/Downloads/gile.jpg'})
+		response = client.post(reverse('register'),
+								 {'username': os.environ['USERNAME'], 
+								 'email': os.environ['EMAIL'], 
+								 'password': os.environ['PASSWORD'],
+								 'password1':os.environ['PASSWORD'],
+								 'orgname': os.environ['ORG'],
+								 'orglogo':'/home/rizwan/Downloads/gile.jpg'})
 		self.assertTrue(response.status_code, 200)
 		self.assertContains(response,'{"status": "Error", "message": "please fill the details"}')
 
 	def test_login(self):
-		user1=User.objects.create(username=os.environ['USERNAME'], email=os.environ['EMAIL'],password=os.environ['PASSWORD'],is_active=True)
+		user1=User.objects.create(username=os.environ['USERNAME'],
+		 							email=os.environ['EMAIL'],
+		 							password=os.environ['PASSWORD'],
+		 							is_active=True)
 		user1.set_password(os.environ['PASSWORD'])
 		user1.save()
 		client=Client()
-		response=client.post(reverse('loginresult'),{'username':os.environ['USERNAME'],'password':os.environ['PASSWORD']})
+		response=client.post(reverse('loginresult'),
+							{'username':os.environ['USERNAME'],
+							'password':os.environ['PASSWORD']})
 		self.assertTrue(response.status_code,200)
 		self.assertContains(response,'{"status": "success", "message": "good"}')
 	
 	def test_login_fail(self):
-		user1=User.objects.create(username=os.environ['USERNAME'], email=os.environ['EMAIL'],password=os.environ['PASSWORD'],is_active=True)
+		user1=User.objects.create(username=os.environ['USERNAME'], 
+									email=os.environ['EMAIL'],
+									password=os.environ['PASSWORD'],
+									is_active=True)
 		client=Client()
-		response=client.post(reverse('loginresult'),{'username':os.environ['USERNAME'],'password':os.environ['PASSWORD']})
+		response=client.post(reverse('loginresult'),
+								{'username':os.environ['USERNAME'],
+								'password':os.environ['PASSWORD']})
 		self.assertTrue(response.status_code,200)
 		self.assertContains(response,'{"status": "Error", "message": "Invalid Username And Password"}')
 
@@ -84,7 +114,8 @@ class Test1(TestCase):
 		w=self.create_user()
 		client=Client()
 		email={'email':os.environ['EMAIL_FORGOT']}
-		response=client.post(reverse('forgotpass'),json.dumps(email),content_type="application/json")
+		response=client.post(reverse('forgotpass'),
+						json.dumps(email),content_type="application/json")
 		self.assertTrue(response.status_code,200)
 		self.assertContains(response,'{"status": "success", "message": "Reset password link has been email to your registered email address."}')
 
@@ -92,31 +123,47 @@ class Test1(TestCase):
 		w=self.create_user()
 		client=Client()
 		email={'email':os.environ.get('EMAIL')}
-		response=client.post(reverse('forgotpass'),json.dumps(email),content_type="application/json")
+		response=client.post(reverse('forgotpass'),json.dumps(email),
+								content_type="application/json")
 		self.assertTrue(response.status_code,200)
 		self.assertContains(response,'{"status": "Error", "message": "Invalid email"}')
 	
 	def test_recover_password(self):
-		user1=User.objects.create(username=os.environ['USERNAME'], email=os.environ['EMAIL'],password=os.environ['PASSWORD'],is_active=True)
+		user1=User.objects.create(username=os.environ['USERNAME'],
+								 email=os.environ['EMAIL'],
+								 password=os.environ['PASSWORD'],
+								 is_active=True)
 		user1.set_password(os.environ['PASSWORD'])
 		user1.save()
 		hash1 = str(uuid.uuid1())
-		obj=user1.forgotpassword_set.create(activation_key=hash1,link_time=timezone.now())
+		obj=user1.forgotpassword_set.create(activation_key=hash1,
+									link_time=timezone.now())
 		client=Client()
-		password={'password':os.environ['PASSWORD'],'password1':os.environ['PASSWORD'],'hash':hash1}
-		response=client.post(reverse('recover_password'),json.dumps(password),content_type="application/json")
+		password={'password':os.environ['PASSWORD'],
+									'password1':os.environ['PASSWORD'],
+									'hash':hash1}
+		response=client.post(reverse('recover_password'),
+									json.dumps(password),
+									content_type="application/json")
 		self.assertTrue(response.status_code,200)
 		self.assertContains(response,'{"status": "success", "message": "password updated successfully"}')
 
 	def test_recover_password_fail(self):
-		user1=User.objects.create(username=os.environ['USERNAME'], email=os.environ['EMAIL'],password=os.environ['EMAIL_FORGOT'],is_active=True)
+		user1=User.objects.create(username=os.environ['USERNAME'], 
+								email=os.environ['EMAIL'],
+								password=os.environ['EMAIL_FORGOT'],
+								is_active=True)
 		user1.set_password(os.environ['PASSWORD'])
 		user1.save()
 		hash1 = str(uuid.uuid1())
-		obj=user1.forgotpassword_set.create(activation_key=hash1,link_time=timezone.now())
+		obj=user1.forgotpassword_set.create(activation_key=hash1,
+											link_time=timezone.now())
 		client=Client()
 		hash1='abcdefgh090908hijkl898989898'
-		password={'password':os.environ['PASSWORD'],'password1':os.environ['PASSWORD'],'hash':hash1}
-		response=client.post(reverse('recover_password'),json.dumps(password),content_type="application/json")
+		password={'password':os.environ['PASSWORD'],
+					'password1':os.environ['PASSWORD'],'hash':hash1}
+		response=client.post(reverse('recover_password'),
+									json.dumps(password),
+									content_type="application/json")
 		self.assertTrue(response.status_code,200)
 		self.assertContains(response,'{"status": "Error", "message": "invalid link or token has been expired."}')
