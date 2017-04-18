7506fb3bf4a792adb6e703f17e1797ceb9849294
rizwan <rizwan.shaikh@cuelogic.com>
2017-04-18 10:38:44 +0530

    catagory management

---
+++ project4/project/ebs/admin.py
@@ -0,0 +1,65 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.conf import settings
+from django.core.mail import send_mail
+from django.contrib.auth.models import User
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string, get_template
+from django.utils.html import strip_tags
+from ebs.models import Organisation,Catagories,Blog,Comment,BlogFile
+
+
+
+admin.site.register(Organisation)
+admin.site.register(Comment)
+admin.site.register(BlogFile)
+class OrganisationInline(admin.StackedInline):
+    model = Organisation
+    can_delete = False
+
+
+def mark_inactive(ModelAdmin, request, queryset):
+    """for  adding deactivate action in admin """
+    queryset.update(is_active=False)
+
+
+def mark_active(ModelAdmin, request, queryset):
+    """for adding activate action in admin as well as sending email to all users"""
+    queryset.update(is_active=True)
+    for obj in queryset:
+        subject = 'Welcome to NewsMagzine'
+        html_content = render_to_string(
+            'ebs/welcomemail.html', {'HOST': settings.HOST})
+        text_content = strip_tags(html_content)
+        from_email = settings.EMAIL_HOST_USER
+        msg = EmailMultiAlternatives(
+            subject, text_content, from_email, [obj.email])
+        msg.attach_alternative(html_content, "text/html")
+        msg.send()
+mark_inactive.short_description = "Deactivate selected users"
+mark_active.short_description = "Activate selected users"
+
+
+class UserAdmin(UserAdmin):
+    """for customizing user object table as well as adding actions in admin"""
+    inlines = (OrganisationInline,)
+    list_display = ('username', 'email', 'is_active',
+                    'date_joined', 'display_orgname')
+    ordering = ['username']
+    actions = [mark_inactive, mark_active]
+
+    def display_orgname(self, request):
+        if Organisation.objects.filter(user__id=request.id):
+            return 'Client'
+    display_orgname.short_description = 'Role'
+
+
+class CatagoriesModelAdmin(admin.ModelAdmin):
+    list_display=('name','state')
+    list_filter=['state']
+    search_fields=['name']
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+admin.site.register(Catagories,CatagoriesModelAdmin)
+admin.site.register(Blog)
---
+++ project4/project/ebs/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.home, name="home"),
+    url(r'^register/$', views.register, name='register'),
+    url(r'^loginresult/$', views.loginresult, name='loginresult'),
+    url(r'^forgotpass/$', views.forgotpass, name='forgotpass'),
+    url(r'^log_out/$', views.log_out, name='log_out'),
+    url(r'^newpassword/$', views.newpassword, name='newpassword'),
+    url(r'^recover_password/$', views.recover_password, name='recover_password'),
+   
+]
