d38d85911decef79bc8f8c83a8b813b433ad4697
rizwan <rizwan.shaikh@cuelogic.com>
2017-04-18 11:03:51 +0530

    catagory_management

--- project4/project/ebs/forms.py
+++ project4/project/ebs/forms.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User
 from django import forms
 from django.forms import ModelForm
 from ebs.models import Organisation
+
 class UserForm(forms.ModelForm):
 	password=forms.CharField(label="Password" , 
 					widget=forms.PasswordInput(
