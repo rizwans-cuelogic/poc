ddc1fc20d3820303126c01c19614bb12ce3f206d
rizwan <rizwan.shaikh@cuelogic.com>
2017-04-19 13:16:02 +0530

    add validation

--- project4/project/ebs/static/ebs/js/create_blog.js
+++ project4/project/ebs/static/ebs/js/create_blog.js
@@ -1,4 +1,10 @@
 $(document).ready(function() {
+        $.validator.addMethod('filesize', function(value, element, param) {
+        // param = size (en bytes) 
+        // element = element to validate (<input>)
+        // value = value of the element (file name)
+        return this.optional(element) || (element.files[0].size <= param) 
+    });
         jQuery.validator.setDefaults({
         debug: true,
         success: "valid"
@@ -7,7 +13,8 @@ $(document).ready(function() {
         rules: {
             attachments: {
                 required: false,
-                accept: "image/jpeg,image/png,application/msword,application/pdf,application/vnd.ms-excel"
+                accept: "image/jpeg,image/png,application/msword,application/pdf,application/vnd.ms-excel",
+                filesize: 1048576
             },
 
             image1: {
@@ -20,8 +27,8 @@ $(document).ready(function() {
                 accept: "image/jpeg,image/png,application/msword,application/pdf,application/vnd.ms-excel"
             }
 
-        }
+        },
+        messages: {
+             attachments: "File must be JPG, GIF or PNG, less than 1MB" }
     });       
-
-        
-    });
+});
