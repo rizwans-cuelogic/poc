8bf122cb83c099450815bb4345cfdd21a179d296
rizwan <rizwan.shaikh@cuelogic.com>
2017-04-21 15:29:05 +0530

    Revert "Merge pull request #8 from rizwans-cuelogic/feature_addblog"
    This reverts commit eb1533845713937ca28082424893b0bf7f5e86c6, reversing
    changes made to d05cd12c4727658dc9b8070d8f1c050601802043.

--- project4/project/ebs/static/ebs/js/manage_blog.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {    
-	if(document.getElementById('message')!==null){
-    	$.notify.defaults({ className: "success" })
-        $.notify( 
-            "Blog details saved successfully",
-           { position:"top center" }
-        );
-    }
-});
