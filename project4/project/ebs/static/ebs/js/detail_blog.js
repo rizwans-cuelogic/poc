dacf454e400d41f232d5255769b4882238c6a917
rizwan <rizwan.shaikh@cuelogic.com>
2017-05-18 20:21:10 +0530

    changes done of html

--- project4/project/ebs/static/ebs/js/detail_blog.js
+++ project4/project/ebs/static/ebs/js/detail_blog.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
 	$('.blog-img a').attr('target', '_blank');
 	$('.blog-thumbnail a').attr('target', '_blank');
 	$('.blog-attachments a').attr('target', '_blank');
+	$('.blog-content a').attr('target', '_blank');
 	$('.blog-content > p > img').addClass('img-responsive');
 	$( "hr").last().remove();
 	$('[data-toggle="tooltip"]').tooltip();
